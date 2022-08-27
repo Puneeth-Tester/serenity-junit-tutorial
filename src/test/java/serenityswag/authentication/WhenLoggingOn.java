cd6d323e4946746eed5933f3c8406abddd443cbc
Puneeth <emailtopuneeth@gmail.com>
2022-08-27 22:09:46 +0530

    Initial commit

--- src/test/java/serenityswag/authentication/WhenLoggingOn.java
+++ src/test/java/serenityswag/authentication/WhenLoggingOn.java
@@ -1,5 +1,6 @@
 package serenityswag.authentication;
 
+import net.serenitybdd.core.steps.UIInteractionSteps;
 import net.serenitybdd.junit.runners.SerenityRunner;
 import net.thucydides.core.annotations.Managed;
 import org.junit.Test;
@@ -11,22 +12,35 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 
 @RunWith(SerenityRunner.class)
-public class WhenLoggingOn {
+public class WhenLoggingOn extends UIInteractionSteps {
 
-    @Managed
-    WebDriver driver;
+    /*@Managed
+    WebDriver driver;*/
 
     @Test
     public void usersCanLogOnViaTheHomePage(){
-        driver.get("https://www.saucedemo.com/");
+        //driver.get("https://www.saucedemo.com/");
+        openUrl("https://www.saucedemo.com/");
 
         // Login as a standard user
-        driver.findElement(By.cssSelector("[data-test='username']")).sendKeys("standard_user");
+        /*driver.findElement(By.cssSelector("[data-test='username']")).sendKeys("standard_user");
         driver.findElement(By.cssSelector("[data-test='password']")).sendKeys("secret_sauce");
-        driver.findElement(By.cssSelector("[data-test='login-button']")).click();
+        driver.findElement(By.cssSelector("[data-test='login-button']")).click();*/
+
+        /*find("[data-test='username']").sendKeys("standard_user");
+        find("[data-test='password']").sendKeys("secret_sauce");
+        find("[data-test='login-button']").click();*/
+
+        $("[data-test='username']").sendKeys("standard_user");
+        $("[data-test='password']").sendKeys("secret_sauce");
+        $("[data-test='login-button']").click();
 
         // Should see product catalog
-        assertThat(driver.findElement(By.cssSelector(".title")).getText())
-                .isEqualToIgnoringCase("Products");
+        /*assertThat(driver.findElement(By.cssSelector(".title")).getText())
+                .isEqualToIgnoringCase("Products");*/
+
+        //assertThat(find(".title").getText()).isEqualToIgnoringCase("Products");
+
+        assertThat($(".title").getText()).isEqualToIgnoringCase("Products");
     }
 }
